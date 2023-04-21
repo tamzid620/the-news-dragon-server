0ac38afe785f6ed69f5f5aac376b6803a3f3e94c
tamzid620 <tamzid620@gmail.com>
2023-04-21 15:41:33 +0600

    news.json added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 // const port =  5000;
 
 const categories = require('./data/Categories.json')
+const news = require('./data/news.json')
 
 app.use(cors());
 
@@ -14,6 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/categories/:id' , (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    if(id == 0) {
+        res.send(news)
+    }else {
+        const categoryNews = news.filter(n => (n.category_id === id));
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req , res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req , res) => {
+    const id = req.params.id
+    console.log(id)
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
